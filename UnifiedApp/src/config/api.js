f0a78c1f189b72e07bed8112c556366e14351730
shreyashChitkula <schitkula@gmail.com>
2026-01-25 16:08:24 +0530

    no changes

--- UnifiedApp/src/config/api.js
+++ UnifiedApp/src/config/api.js
@@ -1,3 +1,3 @@
 // Hardcoded IP for your network
-export const API_BASE_URL = 'http://10.42.0.1:5000/api';
+export const API_BASE_URL = 'http://192.168.2.28:5000/api';
 // export const API_BASE_URL = 'https://canteenapp-wsu0.onrender.com/api';
